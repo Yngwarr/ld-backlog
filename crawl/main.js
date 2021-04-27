98ddf17c0c63059e91c5208eaed357e9fb116cab
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-04-27 15:29:19 +0700

    final

--- crawl/main.js
+++ crawl/main.js
@@ -1,5 +1,11 @@
 import * as std from "std";
+import { mkdir } from "os";
 import { fetch } from "minnet.so";
+
+const ROOT = '..'
+
+const HEADER_TEMPLATE = `<!doctype html> <html> <head> <meta charset='utf-8'> <title>Ludum Dare Backlog</title> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <link href='../css/main.css' rel='stylesheet'> <script src='../js/main.js'></script> </head> <body onload='init()'>`;
+const FOOTER_TEMLPLATE = `</body></html>`;
 
 function log(obj) {
     console.log(JSON.stringify(obj, null, 2));
@@ -20,6 +26,14 @@ function nodes(path) {
     return crawl(`/node2/get/${ns}`);
 }
 
+function to_url(label) {
+    return label.replace(/[^0-9]/g, '');
+}
+
+function dirname(path) {
+    return path.split('/').pop().split('.').shift();
+}
+
 function crawlGames(author) {
     let games = new Map();
     const id = crawl(`/node2/walk/1/users/${author}`)['node_id'];
@@ -34,6 +48,17 @@ function crawlEvents() {
         //.forEach(x => { events[x.name] = x.id; });
         .forEach(x => events.set(x.id, x.name));
     return events;
+}
+
+function renderHead(events) {
+    let li = [];
+    for (const [k, v] of events) {
+        li.push(`<li><a href="${to_url(v)}.html">${v.replace(/[^0-9]/g, '')}</a></li>`);
+    }
+    li.sort().reverse();
+    li.unshift('<ul id="events">');
+    li.push('</ul>');
+    return li.join('\n');
 }
 
 function renderEvent(head, es) {
@@ -59,32 +84,49 @@ function main() {
 
     console.log('loading list...');
 
-    const list_file = std.open('./lists/pixel-prophecy.txt', 'r');
-    const list = list_file.readAsString().split('\n').filter(x => x.length > 0);
-    list_file.close();
+    let argv = scriptArgs;
+    argv.shift();
 
-    console.log('preparing map...');
-
-    let games = new Map();
-    for (const k of keys) {
-        games.set(k, {});
-        let curr = games.get(k);
-        for (const auth of list) {
-            curr[auth] = null;
-        }
+    for (const v of argv) {
+        mkdir(`${ROOT}/${dirname(v)}`);
     }
 
-    console.log('filling map with games...');
+    const head = renderHead(events);
 
-    for (const author of list) {
-        const entries = crawlGames(author);
-        for (const [k, v] of entries) {
-            games.get(k)[author] = v;
+    for (const arg of argv) {
+        const list_file = std.open(arg, 'r');
+        const list = list_file.readAsString().split('\n').filter(x => x.length > 0);
+        list_file.close();
+
+        console.log('preparing map...');
+
+        let games = new Map();
+        for (const k of keys) {
+            games.set(k, {});
+            let curr = games.get(k);
+            for (const auth of list) {
+                curr[auth] = null;
+            }
+        }
+
+        console.log('filling map with games...');
+
+        for (const author of list) {
+            const entries = crawlGames(author);
+            for (const [k, v] of entries) {
+                games.get(k)[author] = v;
+            }
+        }
+
+        for (const [id, v] of events) {
+            let f = std.open(`${ROOT}/${dirname(arg)}/${to_url(v)}.html`, 'w');
+            f.puts(HEADER_TEMPLATE);
+            f.puts(head);
+            f.puts(renderEvent(events.get(id), games.get(id)));
+            f.puts(FOOTER_TEMLPLATE);
+            f.close();
         }
     }
-
-    const id = 32802;
-    console.log(renderEvent(events.get(id), games.get(id)));
 }
 
 main();
